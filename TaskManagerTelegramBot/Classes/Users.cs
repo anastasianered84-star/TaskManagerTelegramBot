2ee256d145c52b999bc83dcd9b388310644aca9b
Anastasia <anastasianered84@gmail.com>
2025-12-11 21:28:08 +0500

    Добавьте файлы проекта.

---
+++ TaskManagerTelegramBot/Classes/Users.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerTelegramBot.Classes
+{
+    public class Users
+    {
+        public long IdUser { get; set; }
+        public List<Events> Events {get; set;}
+
+        public Users(long idUser) { 
+        
+            IdUser = idUser;
+            Events = new List<Events>();
+        }
+
+    }
+}
---
+++ TaskManagerTelegramBot/Program.cs
@@ -0,0 +1,10 @@
+using TaskManagerTelegramBot;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
