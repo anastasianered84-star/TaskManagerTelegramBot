04898881d78026c1cc1a7cf06f2d2d7d517ea306
Anastasia <anastasianered84@gmail.com>
2025-12-11 21:41:34 +0500

    Разработанный методы создания кнопок, и проверка на вводимую дату и время. Фефелова

--- TaskManagerTelegramBot/Classes/Events.cs
+++ TaskManagerTelegramBot/Classes/Events.cs
@@ -16,5 +16,7 @@ namespace TaskManagerTelegramBot.Classes
             Time = time;
             Message = message;
         }
+
+       
     }
 }
--- TaskManagerTelegramBot/Worker.cs
+++ TaskManagerTelegramBot/Worker.cs
@@ -1,7 +1,64 @@
+using TaskManagerTelegramBot.Classes;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
 namespace TaskManagerTelegramBot
 {
     public class Worker : BackgroundService
     {
+        readonly string Token = "";
+        TelegramBotClient TelegramBotClient;
+        List<Users> Users = new List<Users>();
+        Timer Timer;
+
+        static List<string> Messages = new List<string>()
+    {
+        "Здравствуйте!" +
+        "\nРады приветствовать вас в Telegram-боте «Напоминатор»!" +
+        "\nНаш бот создан для того, чтобы напоминать вам о важных событиях и мероприятиях. С ним вы точно не пропустите ничего важного! " +
+        "\nНе забудьте добавить бота в список своих контактов и настроить уведомления. Тогда вы всегда будете в курсе событий! ",
+
+        "Укажите дату и время напоминания в следующем формате:" +
+        "\n<i><b>12:51 26.04.2025</b>" +
+        "\nНапомни о том что я хотел сходить в магазин.</i>",
+
+        "Кажется, что-то не получилось." +
+        "Укажите дату и время напоминания в следующем формате:" +
+        "\n<i><b>12:51 26.04.2025</b>" +
+        "\nНапомни о том что я хотел сходить в магазин.</i>",
+
+        "Задачи пользователя не найдены.",
+        "Событие удалено.",
+        "Все события удалены."
+    };
+
+        public bool CheckFormatDateTime(string value, out DateTime time)
+        {
+            return DateTime.TryParse(value, out time);
+        }
+
+        private static ReplyKeyboardMarkup GetButtons()
+        {
+            List<KeyboardButton> keyboardButtons = new List<KeyboardButton>();
+            keyboardButtons.Add(new KeyboardButton("Удалить все задачи"));
+
+            return new ReplyKeyboardMarkup
+            {
+                Keyboard = new List<List < KeyboardButton >>{
+                    keyboardButtons
+                }
+            };
+        }
+
+        public static InlineKeyboardMarkup DeleteEvent(string Message)
+        {
+            List<InlineKeyboardButton> inlineKeyboards = new List<InlineKeyboardButton>();
+            inlineKeyboards.Add(new InlineKeyboardButton("Удалить", Message));
+
+            return new InlineKeyboardMarkup(inlineKeyboards);
+        }
+
         private readonly ILogger<Worker> _logger;
 
         public Worker(ILogger<Worker> logger)
@@ -17,5 +74,7 @@ namespace TaskManagerTelegramBot
                 await Task.Delay(1000, stoppingToken);
             }
         }
+
+
     }
 }
